d1b7c530ce4007f6cf59b75fefd671cf2f51e46a
Christoph Böhner-Figas <boehner@team23.de>
2018-06-11 17:55:03 +0200

    Update fabric integration

--- fabfile.py
+++ fabfile.py
@@ -1,55 +1,39 @@
+import os
+import getpass
 from fabric.api import cd
 from fabric.api import env
 from fabric.api import local
 from fabric.api import run
 from fabric.api import task
+from fabric.colors import green
+from fabric.colors import red
 
 from ade25.fabfiles import project
+from ade25.fabfiles.server import setup
 from ade25.fabfiles.server import controls
 from ade25.fabfiles import hotfix as hf
+
+from slacker import Slacker
+slack = Slacker('xoxp-2440800772-2440800774-136782824743-8ab4ddccedc95a8deb53d6cde10d9976')
 
 env.use_ssh_config = True
 env.forward_agent = True
 env.port = '22222'
 env.user = 'root'
+env.hostname = 'zope1'
 env.code_user = 'root'
 env.prod_user = 'www'
 env.webserver = '/opt/webserver/buildout.webserver'
 env.code_root = '/opt/webserver/buildout.webserver'
 env.host_root = '/opt/sites'
+env.local_dir = '/Users/cb/ops/z1'
 
-env.hosts = ['zope1']
-env.hosted_sites = [
-    'asg',
-    'reiter',
-    'teama3',
-    'ahlt',
-    'androschin',
-    'dichtl',
-    'existenz',
-    'rms',
-    'langer',
-    'anna',
-]
+env.hosts = ['zope1.kreativkombinat.de']
+env.hosted_sites = {
+}
 
 env.hosted_sites_locations = [
-    '/opt/sites/asg/buildout.asg',
-    '/opt/sites/reiter/buildout.reiter',
-    '/opt/sites/teama3/buildout.teama3',
-    '/opt/sites/ahlt/buildout.ahlt',
-    '/opt/sites/androschin/buildout.androschin',
-    '/opt/sites/dichtl/buildout.dichtl',
-    '/opt/sites/existenz/buildout.existenz',
-    '/opt/sites/rms/buildout.rms',
-    '/opt/sites/langer/buildout.langer',
-    '/opt/sites/anna/buildout.anna',
 ]
-
-
-@task
-def push():
-    """ Push committed local changes to git """
-    local('git push')
 
 
 @task
@@ -63,13 +47,6 @@ def restart():
 def restart_nginx():
     """ Restart Nginx """
     controls.restart_nginx()
-
-
-@task
-def nginx(*cmd):
-    """Run Nginx command (start|stop|status)"""
-    with cd(env.webserver):
-        run('nice bin/supervisorctl ' + ' '.join(cmd) + ' nginx')
 
 
 @task
@@ -92,27 +69,76 @@ def ctl(*cmd):
 
 
 @task
-def supervisorctl(*cmd):
-    """Runs an arbitrary supervisorctl command."""
-    with cd(env.webserver):
-        run('nice bin/supervisorctl ' + ' '.join(cmd))
+def prepare_deploy():
+    """ Push committed local changes to git """
+    local('git push')
+
+@task
+def add_site(site_id=None):
+    opts = dict(
+        filename=os.path.join(
+            env.local_dir, 'buildout.d', 'templates', 'nginx.conf'
+        ),
+        replacement='site{0}.conf;'.format(site_id)
+    )
+    cmd = r"sed -i '' '/%(replacement)s/s/^#*//g' %(filename)s " % opts
+    local(cmd)
+    print(green('site{0} has been activated'.format(site_id)))
 
 
 @task
-def deploy():
+def remove_site(site_id=None):
+    opts = dict(
+        filename=os.path.join(
+            env.local_dir, 'buildout.d', 'templates', 'nginx.conf'
+        ),
+        replacement='site{0}.conf;'.format(site_id)
+    )
+    cmd = r"sed -i '' '/%(replacement)s/s/^/#/g' %(filename)s " % opts
+    local(cmd)
+    print(red('site{0} has been deactivated'.format(site_id)))
+
+
+@task
+def add_cert(servername=None):
+    setup.certbot(servername)
+    setup.certbot('www.{0}'.format(servername))
+
+
+@task
+def deploy(actor=None):
     """ Deploy current master to production server """
-    push()
-    controls.update()
-    controls.build()
+    opts = dict(
+        actor=actor or env.get('actor') or getpass.getuser(),
+    )
+    project.site.update()
+    project.site.build()
+    with cd(env.webserver):
+        run('bin/supervisorctl reread')
+        run('bin/supervisorctl update')
+    msg = '[zope1] zope1.kreativkombinat.de server configuration deployed by %(actor)s' % opts
+    user = 'fabric'
+    icon = ':shipit:'
+    slack.chat.post_message('#development', msg, username=user, icon_emoji=icon)
 
 
 @task
-def deploy_site():
-    """ Deploy a new site to production """
-    push()
-    controls.update()
-    controls.build()
-    controls.reload_supervisor()
+def update(sitename=None, actor=None):
+    """ Deploy changes to a hosted site """
+    opts = dict(
+        sitename=sitename,
+        actor=actor or env.get('actor') or getpass.getuser(),
+    )
+    path = '{0}/{1}/buildout.{2}'.format(env.host_root, sitename, sitename)
+    with cd(path):
+        run('nice git pull')
+        run('nice bin/buildout -Nc deployment.cfg')
+    with cd(env.webserver):
+        run('nice bin/supervisorctl restart instance-%(sitename)s' % opts)
+    msg = '[zope1] %(sitename)s deployed by %(actor)s' % opts
+    user = 'fabric'
+    icon = ':shipit:'
+    slack.chat.post_message('#development', msg, username=user, icon_emoji=icon)
 
 
 @task
